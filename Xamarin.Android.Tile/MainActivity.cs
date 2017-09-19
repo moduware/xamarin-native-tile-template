8a0d122868d1840705d6b854a89bf6fc06ee3b99
Alex Chernov <alexusblack@gmail.com>
2017-09-19 15:33:29 +1000

    COnfiguring core

--- Xamarin.Android.Tile/MainActivity.cs
+++ Xamarin.Android.Tile/MainActivity.cs
@@ -1,30 +1,43 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Platform.Core;
 using Plugin.BLE;
+using Platform.Core;
+using Serilog;
+using System.Threading.Tasks;
 
 namespace Xamarin.Android.Tile
 {
-    [Activity(Label = "Xamarin.Android.Tile", MainLauncher = true)]
+    [Activity(Label = "Tile template", MainLauncher = true)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            // Logger to output messages from PlatformCore to console
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.AndroidLog()
+                .CreateLogger();
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Loading Platform Core
-            var Core = new Core(code => RunOnUiThread(code), PassiveMode: true, settings: new CoreSettings
+            Task.Run(() =>
             {
-                RequestManifests = true,
-                // TODO: make this setting true by default, as it is used like this in both Tile and App
-            });
+                // Loading Platform Core
+                var Core = new Core(code => RunOnUiThread(code), PassiveMode: true, settings: new CoreSettings
+                {
+                    RequestManifests = true,
+                    // TODO: make this setting true by default, as it is used like this in both Tile and App
+                });
 
-            // Searching for connected gateways
-            Core.Gateways.CheckConnected();
+                Core.Error += (sender, e) => Log.Information("[PlatformCore] Error: " + e.Message);
+
+                // Searching for connected gateways
+                Core.Gateways.CheckConnected();
+            });
+            
 
         }
     }
