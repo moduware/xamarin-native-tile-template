c9819fdd4de6e4a14a90f468cb89b25cbb86a720
Alex Chernov <alexusblack@gmail.com>
2017-09-28 10:02:42 +1000

    iOS bare native tile

---
+++ Xamarin.iOS.Tile/RootViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using UIKit;
+using Platform.Core;
+using Plugin.BLE;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Xamarin.iOS.Tile
+{
+    public partial class RootViewController : UIViewController
+    {
+        private Core Core;
+
+        public RootViewController() : base("RootViewController", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Task.Run(() =>
+            {
+                // Perform any additional setup after loading the view, typically from a nib.
+                Core = new Core((code) => InvokeOnMainThread(code), PassiveMode: true, settings: new CoreSettings
+                {
+                    RequestManifests = true,
+                    RequestModuleDrivers = true
+                });
+                
+                Core.Error += (sender, e) => Log.Information("[PlatformCore] Error: " + e.Message);
+
+                // Searching for connected gateways
+                Core.Gateways.CheckConnected();
+            });
+        }
+
+        partial void SetColorButton_TouchUpInside(UIButton sender)
+        {
+            var RedNumber = int.Parse(RedColor.Text);
+            var GreenNumber = int.Parse(GreenColor.Text);
+            var BlueNumber = int.Parse(BlueColor.Text);
+
+            // finding all LED modules
+            foreach (var module in Core.Gateways.List[0].Modules)
+            {
+                if (module == null) continue;
+                if (module.Manifest.TypeID == "nexpaq.module.led")
+                {
+                    Core.API.Module.SendCommand(module.UUID, "SetRGB", new[] { RedNumber, GreenNumber, BlueNumber });
+                }
+            }
+        }
+    }
+}
---
+++ Xamarin.iOS.Tile/RootViewController.designer.cs
@@ -0,0 +1,59 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Xamarin.iOS.Tile
+{
+    [Register ("RootViewController")]
+    partial class RootViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField BlueColor { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField GreenColor { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField RedColor { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SetColorButton { get; set; }
+
+        [Action ("SetColorButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void SetColorButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (BlueColor != null) {
+                BlueColor.Dispose ();
+                BlueColor = null;
+            }
+
+            if (GreenColor != null) {
+                GreenColor.Dispose ();
+                GreenColor = null;
+            }
+
+            if (RedColor != null) {
+                RedColor.Dispose ();
+                RedColor = null;
+            }
+
+            if (SetColorButton != null) {
+                SetColorButton.Dispose ();
+                SetColorButton = null;
+            }
+        }
+    }
+}
