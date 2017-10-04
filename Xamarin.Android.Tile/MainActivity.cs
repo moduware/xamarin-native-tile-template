d4da7c2ca1b9f9f8c52f1db2feaa6d9e317961f8
Alex Chernov <alexusblack@gmail.com>
2017-10-04 15:08:27 +1100

    something template like for android

--- Xamarin.Android.Tile/MainActivity.cs
+++ Xamarin.Android.Tile/MainActivity.cs
@@ -7,6 +7,7 @@ using Serilog;
 using System.Threading.Tasks;
 using Platform.Core.CommonTypes;
 using Xamarin.Tile.Structs;
+using System;
 
 namespace Xamarin.Android.Tile
 {
@@ -15,6 +16,7 @@ namespace Xamarin.Android.Tile
     public class MainActivity : Activity
     {
         public TileArguments Arguments = new TileArguments();
+        private Core Core;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -25,20 +27,45 @@ namespace Xamarin.Android.Tile
                 .WriteTo.AndroidLog()
                 .CreateLogger();
 
-            if(Intent.Data != null && Intent.Data.Host == "index")
-            {
-                Arguments = new TileArguments
-                {
-                    TargetModuleUuid = new Uuid(Intent.Data.GetQueryParameter("target-module-uuid")),
-                    TargetModuleSlot = int.Parse(Intent.Data.GetQueryParameter("target-module-slot")),
-                    TargetModuleType = Intent.Data.GetQueryParameter("target-module-type")
-                };
-            }
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            Core Core = null;
+            // Receiving arguments for tile
+            if (Intent.Data != null)
+            {
+                if (Intent.Data.Host == "index")
+                {
+                    Arguments = new TileArguments
+                    {
+                        TargetModuleUuid = new Uuid(Intent.Data.GetQueryParameter("target-module-uuid")),
+                        TargetModuleSlot = int.Parse(Intent.Data.GetQueryParameter("target-module-slot")),
+                        TargetModuleType = Intent.Data.GetQueryParameter("target-module-type")
+                    };
+                }
+            }
+
+            // Launching core in separate thread from UI
+            Task.Run(async () =>
+            {
+                // Loading Platform Core
+                Core = new Core(code => RunOnUiThread(code), PassiveMode: true);
+                // Handling errors happening in core of native tile
+                // TODO: Switch to new events manager instead
+                Core.Error += (sender, e) => Log.Information("[PlatformCore] Error: " + e.Message);
+
+                // Searching for connected gateways
+                var connected = await Core.Gateways.CheckConnected();
+
+                if(!connected)
+                {
+                    // let user know that there are no connected gateways and it is required to open Moduware app for connection
+                    ShowNotConnectedAlert(() =>
+                    {
+                        // TODO: open moduware application
+                    });
+                }
+            });
+
 
             var ConfigButton = FindViewById<Button>(Resource.Id.button1);
             var RedEditbox = FindViewById<EditText>(Resource.Id.editText1);
@@ -52,36 +79,73 @@ namespace Xamarin.Android.Tile
                 var GreenNumber = int.Parse(GreenEditbox.Text);
                 var BlueNumber = int.Parse(BlueEditbox.Text);
 
-                // finding all LED modules
-                foreach(var module in Core.Gateways.List[0].Modules)
+                Uuid targetUuid = GetTargetModuleOrFirstOfType("nexpaq.module.led");
+
+                // Running command on found module
+                if (targetUuid != Uuid.Empty)
                 {
-                    if (module == null) continue;
-                    if (module.Manifest.TypeID == "nexpaq.module.led")
-                    {
-                        Core.API.Module.SendCommand(module.UUID, "SetRGB", new[] { RedNumber, GreenNumber, BlueNumber });
-                    }
+                    Core.API.Module.SendCommand(targetUuid, "SetRGB", new[] { RedNumber, GreenNumber, BlueNumber });
                 }
             };
+        }
 
-            
-
-            Task.Run(async () =>
-            {
-                // Loading Platform Core
-                Core = new Core(code => RunOnUiThread(code), PassiveMode: true, settings: new CoreSettings
-                {
-                    RequestManifests = true,
-                    RequestModuleDrivers = true
-                    // TODO: make this setting true by default, as it is used like this in both Tile and App
-                });
-
-                Core.Error += (sender, e) => Log.Information("[PlatformCore] Error: " + e.Message);
-
-                // Searching for connected gateways
-                await Core.Gateways.CheckConnected();
+        private void ShowNotConnectedAlert(Action callback)
+        {
+            int DialogsTheme = 5;
+            var DialogBuilder = new AlertDialog.Builder(this, DialogsTheme);
+            DialogBuilder.SetTitle("Not connected");
+            DialogBuilder.SetMessage("You are not connected to any moduware device, please search and connect to one.");
+            DialogBuilder.SetCancelable(false);
+            DialogBuilder.SetPositiveButton("Ok", (sender, e) => {
+                callback();
             });
-            
+           
+            RunOnUiThread(action: () => {
+                var AlertDialog = DialogBuilder.Create();
+                AlertDialog.Show();
+            });
+        }
 
+        /// <summary>
+        /// Help function to search for first module of specific type
+        /// </summary>
+        /// <param name="type">Module type</param>
+        /// <returns>module or null if not found</returns>
+        private Platform.Core.Product.Module GetFirstModuleByType(string type)
+        {
+            foreach(var gateway in Core.Gateways.List)
+            {
+                foreach(var module in gateway.Modules)
+                {
+                    if (module == null) continue;
+                    if (module.TypeID == type) return module;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns UUID of target module, if not specified will try to search for supported connected module.
+        /// Will return empty UUID if there are no suitable module found.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private Uuid GetTargetModuleOrFirstOfType(string type)
+        {
+            if (Arguments.TargetModuleUuid != Uuid.Empty)
+            {
+                return Arguments.TargetModuleUuid;
+            }
+            else
+            {
+                var module = GetFirstModuleByType(type);
+                if (module != null)
+                {
+                    return module.UUID;
+                }
+            }
+
+            return Uuid.Empty;
         }
     }
 }
