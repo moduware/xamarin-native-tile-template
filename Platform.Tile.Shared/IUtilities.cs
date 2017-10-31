183816278e864269b9e8c0ccf80b39c49c67815e
Alex Chernov <alexusblack@gmail.com>
2017-10-31 15:55:21 +1100

    Sharing code via shared project instead PCL

---
+++ Platform.Tile.Shared/IUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Platform.Tile.Shared
+{
+    public interface IUtilities
+    {
+        void ShowNotConnectedAlert(Action callback);
+        void OpenDashboard(string request = "");
+    }
+}
---
+++ Platform.Tile.Shared/TileShared.cs
@@ -0,0 +1,190 @@
+﻿using Flurl;
+using Platform.Core;
+using Platform.Core.CommonTypes;
+using Platform.Tile.Shared;
+using Platfrom.Tile.Shared.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if __IOS__
+namespace Platform.Tile.iOS
+{ 
+    public partial class TileViewController
+#endif
+#if __ANDROID__
+namespace Platform.Tile.Droid
+{ 
+    public partial class TileActivity : Android.App.Activity
+#endif
+    {
+        protected string TileId;
+        protected TileArguments Arguments = new TileArguments();
+        protected Core.Core Core;
+        protected IUtilities Utilities;
+
+        private string CurrentConfiguration = String.Empty;
+
+        private void OnCreateActions()
+        {
+            RunCore();
+        }
+
+        private void OnResumeActions()
+        {
+            if (Core != null && Core.Gateways.List.Count == 0)
+            {
+                CheckConnectedGateways();
+            }
+        }
+
+        private void OnQueryRecieved(string queryUrl)
+        {
+            var url = new Url(queryUrl);
+            var domain = url.Path.Replace(TileId + "://", String.Empty);
+            // For index we are getting full set of arguments and configuration
+            if (domain == "index")
+            {
+                Arguments = new TileArguments
+                {
+                    TargetModuleUuid = new Uuid(url.QueryParams["target-module-uuid"].ToString()),
+                    TargetModuleSlot = int.Parse(url.QueryParams["target-module-slot"].ToString()),
+                    TargetModuleType = url.QueryParams["target-module-type"].ToString()
+                };
+
+                // At this step Core 99% not yet loaded, so we are saving config and core will use it when ready
+                CurrentConfiguration = url.QueryParams["current-configuration"].ToString();
+            } // For configure we are receiving only configuration from main app
+            else if (domain == "configure")
+            {
+                // Loading configuration in core, merging known devices with information about them
+                var configuration = url.QueryParams["current-configuration"].ToString();
+                Core.API.MergeConfig(configuration);
+            }
+        }
+
+        private void RunCore()
+        {
+            // Running core in separate thread
+            Task.Run(() =>
+            {
+                // Loading Platform Core
+                Core = new Core.Core(code => RunOnUiThread(code), PassiveMode: true);
+
+                Core.Gateways.GatewayConnected += Gateways_GatewayConnected;
+                Core.Gateways.GatewayDisconnected += Gateways_GatewayDisconnected;
+
+                CheckConnectedGateways();
+            });
+        }
+
+        /// <summary>
+        /// Actions to perfom when we are not connected to any gateway.
+        /// Notify user and open moduware app.
+        /// </summary>
+        protected void NotConnectedActions()
+        {
+            // let user know that there are no connected gateways and it is required to open Moduware app for connection
+            Utilities.ShowNotConnectedAlert(() =>
+            {
+                // open moduware application
+                Utilities.OpenDashboard();
+            });
+        }
+
+        /// <summary>
+        /// Check if we have any connected gateways
+        /// </summary>
+        private void CheckConnectedGateways()
+        {
+            Task.Run(async () =>
+            {
+                var connected = await Core.Gateways.CheckConnected();
+
+                if (!connected)
+                {
+                    NotConnectedActions();
+                }
+            });
+        }
+
+        /// <summary>
+        /// When gateway connected start waiting for it's initialization
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Gateways_GatewayConnected(object sender, Core.EventArguments.GatewayEventArgs e)
+        {
+            e.Gateway.Initialized += Gateway_Initialized;
+        }
+
+        /// <summary>
+        /// If gateway disconnected, check if it was last one
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Gateways_GatewayDisconnected(object sender, Core.EventArguments.ProductIdEventArgs e)
+        {
+            if (Core.Gateways.List.Count == 0)
+            {
+                NotConnectedActions();
+            }
+
+        }
+
+        /// <summary>
+        /// After gateway initialized we need request configuration or apply one received with arguments
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Gateway_Initialized(object sender, EventArgs e)
+        {
+            // TODO: Checking if all gateways initialized
+            // For now we expect only one gateway
+            if (CurrentConfiguration == String.Empty)
+            {
+                RequestCurrentConfiguration();
+            }
+            else
+            {
+                // Using configuration from arguments
+                Core.API.MergeConfig(CurrentConfiguration);
+                CurrentConfiguration = String.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Request current configuration of gateway and modules
+        /// </summary>
+        public void RequestCurrentConfiguration()
+        {
+            Utilities.OpenDashboard($"index?tile-id={TileId}&action=getConfiguration");
+        }
+
+        /// <summary>
+        /// Returns UUID of target module, if not specified will try to search for supported connected module.
+        /// Will return empty UUID if there are no suitable module found.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        protected Uuid GetUuidOfTargetModuleOrFirstOfType(string type)
+        {
+            if (Arguments.TargetModuleUuid != Uuid.Empty)
+            {
+                return Arguments.TargetModuleUuid;
+            }
+            else
+            {
+                var module = Core.API.Module.GetFirstByType(type);
+                if (module != null)
+                {
+                    return module.UUID;
+                }
+            }
+
+            return Uuid.Empty;
+        }
+    }
+}
